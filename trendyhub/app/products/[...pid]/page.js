0ca0475b9b2e5ca9bbf32781c81b0f4c44132921
AnkushS27 <ankush27singh11@gmail.com>
2023-05-27 18:36:31 +0530

    Designing of Product Card

--- trendyhub/app/products/[...pid]/page.js
+++ trendyhub/app/products/[...pid]/page.js
@@ -22,12 +22,12 @@ export default async function Page({ params }) {
         Stock: {data['stock']} <br />
         Price: {data['price']} <br /> */}
         <div className={Styles.productCard}>
-          <div className={Styles.productImage}> An Image </div>
-          <div className={Styles.productName}> {data['name']} </div>
-          <div className={Styles.productPrice}> ${data['price']} </div>
+          <img src="https://img.freepik.com/free-psd/product-display-3d-podium-background_47987-11310.jpg?w=740&t=st=1685000605~exp=1685001205~hmac=6d697197fe9dfc5be9090aa9091d5ae0e4f0f925975677de4d1b02bf47988686" className={Styles.productImage}></img>
+          <h3 className={Styles.productName}> {data['name']} </h3>
+          <h4 className={Styles.productPrice}> ${data['price']} </h4>
           <div className={Styles.productButtons}>
-            <div className={Styles.btn}> 🛒 </div>
-            <div className={Styles.btn}> 💖 </div>
+            <div className={Styles.cartBtn} data-tooltip="Add to cart"> 🛒 </div>
+            <div className={Styles.wishlistBtn} data-tooltip="Add to Wishlist"> 💖 </div>
           </div>
         </div>
       </div>
