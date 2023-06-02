ec88d98df7095879c6ee813624b79126a5f0274f
Anirudh3167 <77240291+Anirudh3167@users.noreply.github.com>
2023-06-02 22:40:15 +0530

    Home page items are in action

--- trendyhub/app/api/product/route.js
+++ trendyhub/app/api/product/route.js
@@ -3,22 +3,11 @@ import { promises as fs } from 'fs';
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
-    const data = req.nextUrl.searchParams;
-    const pid = data.get('pid');
-
     //Find the absolute path of the json directory
     const jsonDirectory = path.join(process.cwd(), 'DBMS');
     //Read the json data file data.json
-    const fileContents = await fs.readFile(jsonDirectory + '/products.json', 'utf8');
-    const dbms_data = JSON.parse(fileContents);
+    let fileContents = await fs.readFile(jsonDirectory + '/products.json', 'utf8');
 
-    for(let i = 0; i < dbms_data['records'].length; i++) {
-        if (dbms_data['records'][i]['id'] === pid) {
-            let response = new NextResponse(JSON.stringify(dbms_data['records'][i]),{status:200});
-            return response;
-        }
-    }
-
-    let response = new NextResponse("Not Found",{status:200});
+    let response = new NextResponse(fileContents,{status:200});
     return response;
   }
