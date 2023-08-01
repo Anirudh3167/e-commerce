1422308e84e43b7b13ab1b5a972d055469d37be8
Anirudh3167 <77240291+Anirudh3167@users.noreply.github.com>
2023-08-01 12:34:17 +0530

    Navbar is responsive now

--- trendyhub/components/Navbar/page.tsx
+++ trendyhub/components/Navbar/page.tsx
@@ -12,65 +12,84 @@ function Navbar() {
 
   // Functions
   const searchBarAction = (event: ChangeEvent<HTMLInputElement>) => {
+    // To show the relevant searches
   setSearchBarInput(event.target.value);
 };
+  const searchAction = () => {
+    if (searchBarInput !== "") {
+
+      console.log(searchBarInput);
+      setSearchBarInput("");
+    }
+  }
   return (
     <div className={styles.mainWrapper}>
        <div className={styles.mainContainer}>
-          {/* Hamburger Menu */}
-          <div className={`${styles.hamburgerMenu} ${activeHamubrger ? styles.activeHamburger : ""}`}  onClick={() => {seActiveHamburger(!activeHamubrger)}}>
-            <div className={`${styles.hamburgerLine} ${styles.hamburgerTop}`}></div>
-            <div className={`${styles.hamburgerLine} ${styles.hamburgerMiddle}`}></div>
-            <div className={`${styles.hamburgerLine} ${styles.hamburgerBottom}`}></div>
-          </div>
-
           {/* Logo */}
           <div className={styles.logoSpace}> TrendyHub </div>
-
           
-          {/* searchBar */}
-          <input placeholder='Search here' value={searchBarInput} onInput={(e: ChangeEvent<HTMLInputElement>) => {searchBarAction(e)}} className={styles.searchBarBox} />
+          
+            {/* Hamburger Menu */}
+            <div className={`${styles.hamburgerMenu} ${activeHamubrger ? styles.activeHamburger : ""}`}  onClick={() => {seActiveHamburger(!activeHamubrger)}}>
+              <div className={`${styles.hamburgerLine} ${styles.hamburgerTop}`}></div>
+              <div className={`${styles.hamburgerLine} ${styles.hamburgerMiddle}`}></div>
+              <div className={`${styles.hamburgerLine} ${styles.hamburgerBottom}`}></div>
+            </div>  
 
-          {/* Links */}
-          <div className={styles.linksHolder}>
-            <div className={styles.linkContainer}>
-              <Link href="#" className={styles.navLink}> Home </Link>
+          <div className={styles.hamburgerWrapper}>
+            {/* searchBar */}
+            <div className={styles.searchBarContainer}>
+              <input placeholder='Search here' value={searchBarInput} onInput={(e: ChangeEvent<HTMLInputElement>) => {searchBarAction(e)}} className={styles.searchBarBox} />
+              <div className={styles.searchBtn} onClick={() => {searchAction();}}> Search </div>
             </div>
-            <div className={styles.linkContainer}>
-              <Link href="#" className={styles.navLink}> About </Link>
-            </div>
-            <div className={styles.linkContainer}>
-              <div className={styles.navLink}> Deals </div>
-              <div className={styles.subLinksHolder}>
-                <div className={styles.subLinkContainer}>
-                  <Link href="#" className={styles.navLink}> Product 1 </Link>
-                </div>
-                <div className={styles.subLinkContainer}>
-                  <Link href="#" className={styles.navLink}> product 2 </Link>
-                </div>
-                <div className={styles.subLinkContainer}>
-                  <Link href="#" className={styles.navLink}> product 3 </Link>
+
+            {/* Links */}
+            <div className={styles.linksHolder}>
+
+              <div className={styles.linkContainer}>
+                <Link href="/" className={styles.navLink} > Home </Link>
+              </div>
+
+              <div className={styles.linkContainer}>
+                <Link href="#" className={styles.navLink}> About </Link>
+              </div>
+
+              <div className={styles.linkContainer}>
+                <div className={styles.navLink}> Deals </div>
+                <div className={styles.subLinksHolder}>
+                  <div className={styles.subLinkContainer}>
+                    <Link href="#" className={styles.navLink}> Product 1 </Link>
+                  </div>
+                  <div className={styles.subLinkContainer}>
+                    <Link href="#" className={styles.navLink}> product 2 </Link>
+                  </div>
+                  <div className={styles.subLinkContainer}>
+                    <Link href="#" className={styles.navLink}> product 3 </Link>
+                  </div>
                 </div>
               </div>
+
+              <div className={styles.linkContainer}>
+                <div className={`${styles.navLink} ${login ? "" : styles.loginBtn}`}  onClick={() => {login ? "" : setLogin(!login)}}> {login ? "Profile" : "Login / Signup"} </div>
+                {login ?
+                <div className={styles.subLinksHolder}>
+                  <div className={styles.subLinkContainer}>
+                    <Link href="#" className={styles.navLink}> Orders </Link>
+                  </div>
+                  <div className={styles.subLinkContainer}>
+                    <Link href="#" className={styles.navLink}> Wishlist </Link>
+                  </div>
+                  <div className={styles.subLinkContainer}>
+                    <Link href="#" className={styles.navLink}> Settings </Link>
+                  </div>
+                  <div className={styles.subLinkContainer} onClick={() => {setLogin(!login)}}>
+                    <div className={`${styles.navLink} ${styles.logoutBtn}`}> logout </div>
+                  </div>
+                </div> : ""}
+              </div>
+
             </div>
-            <div className={styles.linkContainer}>
-              <div className={`${styles.navLink} ${login ? "" : styles.loginBtn}`}  onClick={() => {login ? "" : setLogin(!login)}}> {login ? "Profile" : "Login / Signup"} </div>
-              {login ?
-              <div className={styles.subLinksHolder}>
-                <div className={styles.subLinkContainer}>
-                  <Link href="#" className={styles.navLink}> Orders </Link>
-                </div>
-                <div className={styles.subLinkContainer}>
-                  <Link href="#" className={styles.navLink}> Wishlist </Link>
-                </div>
-                <div className={styles.subLinkContainer}>
-                  <Link href="#" className={styles.navLink}> Settings </Link>
-                </div>
-                <div className={styles.subLinkContainer} onClick={() => {setLogin(!login)}}>
-                  <div className={`${styles.navLink} ${styles.logoutBtn}`}> logout </div>
-                </div>
-              </div> : ""}
-            </div>
+
           </div>
        </div>
     </div>
--- trendyhub/src/app/page.tsx
+++ trendyhub/src/app/page.tsx
@@ -7,7 +7,11 @@ export default function Home() {
     <div className={styles.mainWrapper}>
       <Navbar />
       <div className={styles.mainContainer}>
-        This is a home page.
+        Sliders will be here for categories :- <br />
+        1. Big Slider for Brand Ads <br />
+        2. Slider for Latest <br />
+        3. Slider for Trending <br />
+        4. Slider for Electronics <br />
       </div>
     </div>
   )
