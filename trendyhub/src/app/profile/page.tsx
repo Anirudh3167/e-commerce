8cdeeaf7e5f3beb42ebb667f4c644704344b3e06
Anirudh3167 <77240291+Anirudh3167@users.noreply.github.com>
2023-08-08 12:59:07 +0530

    porfile page created

--- trendyhub/src/app/profile/page.tsx
+++ trendyhub/src/app/profile/page.tsx
@@ -3,12 +3,17 @@ import React, { useState} from 'react'
 
 import styles from './page.module.css'
 import Navbar from '../../../components/Navbar/page'
+import Link from 'next/link'
 
 function Profile() {
   const [userData,setUserData] = useState({
     "fname":"Master","lname":"Master", "username" : "Master",
     "email":"master@gmail.com",
     "phone":"1123456789",
+    "country":"India",
+    "City":"Delhi",
+    "pincode" : "110091",
+    "address" : "Rithala,Delhi,India",
     "profilepic":"",
     "orders":[],
     "wishlist":[],
@@ -20,35 +25,62 @@ function Profile() {
     <div>
         <Navbar />
         <div className={styles.mainContainer}>
-                                                                {/* User Details on Left Side */}
-            <div className={styles.leftContainer}>
-                <button className={styles.editDetails} onClick={() => {setEditDetails(!editDetails)}}> {editDetails ? "save":"Edit"} </button>
-                <div className={styles.userDetailsSection}>
-                    <div className={styles.profilePicContainer}>
+                                                                {/* User Details */}
+            <div className={styles.editDetails} onClick={()=>{setEditDetails(!editDetails)}}>{editDetails ? "save" : "Edit"}</div>
+            <div className={styles.userDetailsWrapper}>
+                <div className={styles.headingItem}> Personal Info </div>
+                <div className={styles.userDetailsContainer}>
+                    <div className={styles.profilePicContainer}>      {/* Left Side */}
                         <div className={styles.profilePic}> M </div>
                         <div className={styles.profileUsername}> @{userData.username} </div>
                     </div>
-                    <div className={styles.profileContent}> 
-                        First Name :
-                        <input type="text" className={styles.InputBox} defaultValue={userData.fname} readOnly={!editDetails} />
+                    <div className={styles.profileDetails}>        {/* Right Side */}
+                        <div className={styles.detailsItem}>
+                            First Name
+                            <input type="text" className={styles.InputBox} defaultValue={userData.fname} readOnly={!editDetails} />
+                        </div>
+                        <div className={styles.detailsItem}>
+                            Last Name
+                            <input type="text" className={styles.InputBox} defaultValue={userData.lname} readOnly={!editDetails} />
+                        </div>
                     </div>
-                    <div className={styles.profileContent}> 
-                        Last Name :
-                        <input type="text" className={styles.InputBox} defaultValue={userData.lname} readOnly={!editDetails} />
+                </div>
+                <div className={styles.otherUserDetails}>
+                    <div className={styles.detailsItem}>
+                        Email
+                        <input type="email" className={styles.InputBox} defaultValue={userData.email} readOnly={true} />
                     </div>
-                    <div className={styles.profileContent}> 
-                        Email :
-                        <input type="email" className={styles.InputBox} defaultValue={userData.email} readOnly={!editDetails} />
+                    <div className={styles.detailsItem}>
+                        Phone
+                        <input type="tel" className={styles.InputBox} defaultValue={userData.phone} readOnly={true} />
                     </div>
-                    <div className={styles.profileContent}> 
-                        Phone :
-                        <input type="tel" className={styles.InputBox} defaultValue={userData.phone} readOnly={!editDetails} />
+                </div>
+                <div className={styles.otherUserDetails}>
+                    <div className={styles.headingItem}> Address </div>
+                    <div className={styles.detailsItem}>
+                        Country:
+                        <input type="text" className={styles.InputBox} defaultValue={userData.country} readOnly={!editDetails} />
+                    </div>
+                    <div className={styles.detailsItem}>
+                        City:
+                        <input type="text" className={styles.InputBox} defaultValue={userData.City} readOnly={!editDetails} />
+                    </div>
+                    <div className={styles.detailsItem}>
+                        pincode:
+                        <input type="number" className={styles.InputBox} defaultValue={userData.pincode} readOnly={!editDetails} />
+                    </div>
+                    <div className={styles.detailsItem}>
+                        Address:
+                        <input type="text" className={styles.InputBox} defaultValue={userData.address} readOnly={!editDetails} />
                     </div>
                 </div>
             </div>
-                                                            {/* Oreders and all on Right Side */}
-            <div className={styles.rightContainer}>
-                No Details Yet.
+
+            <div className={styles.userOptions}>
+                <div className={styles.headingItem}> Others </div>
+                <Link href="#" className={styles.OptionsItem}> Orders </Link>
+                <Link href="#" className={styles.OptionsItem}> Wishlist </Link>
+                <Link href="#" className={styles.OptionsItem} style={{backgroundColor:"red",color:"white"}}> Logout </Link>
             </div>
         </div>
     </div>
