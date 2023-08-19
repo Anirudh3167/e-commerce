64f5a2e3bb592ca7fb6541543a6a2030b8aa8ea6
Anirudh3167 <77240291+Anirudh3167@users.noreply.github.com>
2023-08-19 22:23:25 +0530

    cms login page created

---
+++ trendyhub/src/app/cms/login/page.tsx
@@ -0,0 +1,45 @@
+"use client"
+import React,{ useState } from 'react'
+
+
+import styles from './page.module.css'
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
+
+function page() {
+  const [uid,setUid] = useState("");
+  const [pass,setPass] = useState("");
+  const [showPass,setShowPass] = useState(false);
+
+  const handleSubmit = () => {
+    if (uid === "master" && pass === "master") {
+        alert("You are logged in to CMS");
+    } else {
+        alert("Invalid Credentials");
+    }
+  }
+  return (
+    <div className={styles.LoginWrapper}>
+        <div className={styles.LoginContainer}>
+            <div className={styles.inputContainer}>
+                <div className={styles.inputHead}> User Id </div>
+                <input className={styles.inputBox} type="text" onChange={(e) => {setUid(e.target.value)}} placeholder='username' />
+            </div>
+            <div className={styles.inputContainer}>
+                <div className={styles.inputHead}> Password </div>
+                <div className={styles.passwordContainer}>
+                    <input className={styles.inputBox} type={showPass ? "text" : "password"} onChange={(e) => {setPass(e.target.value)}} placeholder='password' />
+                    <div className={styles.showPass} onClick={()=>{setShowPass(!showPass)}}>
+                        {showPass ?
+                            <AiFillEye />
+                        : <AiFillEyeInvisible />
+                        }
+                    </div>
+                </div>
+            </div>
+            <div className={styles.submitBtn} onClick={()=>{handleSubmit()}}> Submit </div>
+        </div>
+    </div>
+  )
+}
+
+export default page
